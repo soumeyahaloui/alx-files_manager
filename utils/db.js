855a6bcba4298620981e16aa6dc49ec422a188e7
soumeyahaloui <soumeyahaloui@gmail.com>
2024-03-13 04:37:36 +0000

    updated

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+import dbClient from './utils/db';
+
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async () => {
+            await setTimeout(() => {
+                i += 1;
+                if (i >= 10) {
+                    reject(new Error('Connection timeout'));
+                }
+                else if(!dbClient.isAlive()) {
+                    repeatFct();
+                }
+                else {
+                    resolve();
+                }
+            }, 1000);
+        };
+        repeatFct();
+    });
+};
+
+(async () => {
+    try {
+        console.log(dbClient.isAlive());
+        await waitConnection();
+        console.log(dbClient.isAlive());
+        console.log(await dbClient.nbUsers());
+        console.log(await dbClient.nbFiles());
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+})();

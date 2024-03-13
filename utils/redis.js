83c93473fde034e6ec1a7f22c7b2c4f6694fef69
soumeyahaloui <soumeyahaloui@gmail.com>
2024-03-13 03:13:57 +0000

    updated

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,3 @@
-// Import the required Redis module
 import redis from 'redis';
 
 class RedisClient {
@@ -21,20 +20,42 @@ class RedisClient {
     async get(key) {
         return new Promise((resolve, reject) => {
             this.client.get(key, (err, reply) => {
-                if (err) reject(err);
-                else resolve(reply);
+                if (err) {
+                    console.error('Redis error:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
             });
         });
     }
 
     // Store a value in Redis with an expiration time
     async set(key, value, duration) {
-        this.client.setex(key, duration, value);
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) {
+                    console.error('Redis error:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
     }
 
     // Remove a value from Redis for a given key
     async del(key) {
-        this.client.del(key);
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    console.error('Redis error:', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
     }
 }
 
